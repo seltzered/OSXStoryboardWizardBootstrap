232f3727424ea94ed23a1931bd67ed1e9b56ec22
Vivek Gani <vivek@gani.org>
2016-04-27 19:33:01 -0700

    Initial Commit

---
+++ storyboardDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  storyboardDemo
+//
+//  Created by Vivek Gani on 4/27/16.
+//  Copyright © 2016 Vivek Gani. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ storyboardDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  storyboardDemo
+//
+//  Created by Vivek Gani on 4/27/16.
+//  Copyright © 2016 Vivek Gani. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
